b4ff3a3b0a2724f6253df9ccf68183cf2ef700dd
leonardoantonelli <antonellileonardodesign@gmail.com>
2022-12-12 11:20:41 +0100

    alternativa correzione

--- m1/w2/progettosettimanale/D5.js
+++ m1/w2/progettosettimanale/D5.js
@@ -24,13 +24,13 @@ console.log(pets.sort())
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 
 console.log(pets.reverse())*/
+
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
-
+*/
 const pets = ['dog', 'cat', 'hamster', 'redfish']
-for ( i=1; pets.length>i;i++){
-  console.log(pets)
-}*/
+console.log(pets)
+
 
 
 /* ESERCIZIO 5
@@ -74,13 +74,21 @@ console.log(cars)
 for (let i = 0; i < cars.length; i++) {
   cars[i].trims.splice( cars[i].trims.length -1, 1);
 }
+//SECONDO METODO
+for (let i = 0; i < cars.length; i++) {
+  cars[i].trims.pop()
+console.log(cars[i].trims)}
+
 
 
 
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
-*/
-const justTrims = []
+*/const justTrims = []
+for (let i = 0; i < cars.length; i++) {
+  cars[i].trims[0].push(justTrims)}
+  console.log(justTrims)
+
 
 
 /* ESERCIZIO 8
@@ -95,21 +103,30 @@ if(cars1.color )
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ] 
-/*
+/* no
 while (numericArray[] <32) {
   console.log(numericArray)
   numericArray= numericArray +1
   if (numericArray === 32) break
 }*/
-
+// si
 for (i = 0; i < [13]; i++) {
   console.log(numericArray[i])}
 
 
-/* ESERCIZIO 10
+/* ESERCIZIO 10 V
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
     es. [f, b, e] --> [6, 2, 5]
 
 const charactersArray = ["g", "n"," u", "z", "d"]
 */
+// con la funzione creo una variabile anum e gli assegno ad una lettera un numero.  
+function letterValue(str){
+  var anum={
+      a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, 
+      l: 12, m: 13, n: 14,o: 15, p: 16, q: 17, r: 18, s: 19, t: 20, 
+      u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
+  }
+  if(str.length== 1) return anum[str] || ' ';
+  return str.split('').map(letterValue);}
