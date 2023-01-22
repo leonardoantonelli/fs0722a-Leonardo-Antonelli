d1984e2bd3f086249796beed3b16e80f1a9f539d
Leonardo Antonelli <116543466+leonardoantonelli@users.noreply.github.com>
2023-01-23 00:29:20 +0100

    Delete m2/w2/progetto settimanale/progetto settimanale directory

--- m2/w2/progetto settimanale/progetto settimanale/javas.js
+++
@@ -1,7 +0,0 @@
-var toggleBtn = document.getElementById('toggle-btn');
-    toggleBtn.onclick = function() {
-        this.classList.toggle('toggle-btn--dark');
-        document.body.classList.toggle('dark-mode');
-        document.getElementById("demo").innerHTML = '<img class="divisorio" src="./assets/img/home/marro.png">';
-
-    };
